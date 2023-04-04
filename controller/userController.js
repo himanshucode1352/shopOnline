582e15a59537c223a9e7771ed8361b9ce1406380
himanshu_tripathi tripathi <himtri6657@gmail.com>
2023-04-04 18:23:34 +0000

    changes in user controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js"
 import bcrypt from "bcrypt"
- export const createUser = async (req,res) =>{
+  const createUser = async (req,res) =>{
     console.log(req.body)
     try {
         const saltRound = 10;
@@ -16,6 +16,30 @@ import bcrypt from "bcrypt"
     }
     }
 
-    const login = ()=>{
-        
+    const login = async(req,res)=>{
+    try {
+        const{email,password}=req.body;
+        const existingUser = await User.findOne({ email: email });
+        if (!existingUser) {
+          return res.status(404).send({ message: "User not found" });
+
+
+          
+        }
+    
+        const matchedPassword = await bcrypt.compare(
+          password,
+          existingUser.password
+        );
+    
+        if (!matchedPassword) {
+          return res.status(400).json({ message: "Invalid password" });
+        }
+    
+    
+    } catch (error) {
+        res.status(500).send({msg:error.message});
     }
+   
+    }
+    export  {login,createUser }
