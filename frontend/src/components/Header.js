df6a3aec82eadac7087422b4c72423a8e9f81ed1
Himanshu <himanshu6657@gmail.com>
2023-05-10 00:37:57 +0530

    register page designed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <>
-     <nav className="navbar navbar-expand-lg bg-body-tertiary">
+     <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
   <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <Link to= "/" className="navbar-brand" href="#">Home</Link >
+      <Link to= "/" className="navbar-brand" >Home</Link >
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link to= "/" className="nav-link active" aria-current="page" href="#">Home</Link >
+          <Link to= "/" className="nav-link " aria-current="page">Home</Link >
         </li>
         <li className="nav-item">
           <Link to= "/register" className="nav-link" >Register</Link >
         </li>
         <li className="nav-item">
-          <Link to= "/login" className="nav-link ">Login</Link >
+          <Link to= "/login" className="nav-link  ">Login</Link >
           </li>
           <li className="nav-item">
           <Link to= "/" className="nav-link ">Cart(0)</Link >
--- frontend/src/screens/Register.js
+++ frontend/src/screens/Register.js
@@ -1,28 +1,77 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
 
 const Register = () => {
   return (
     <>
-    <Layout>
-    <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" />
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-</Layout>
-</>  )
-}
+      <Layout>
+        <div className="register">
+          <h2>Register Here</h2>
+          <form>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                aria-describedby="emailHelp"
+                placeholder="Enter your Name"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="email"
+                className="form-control"
+              
+                placeholder="Enter your Email"
+                required
+              />
+              
+            </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your Address"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Enter your Phone number"
+                required
+              />
+            </div>
+           
+            <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Enter your password"
+                  required
+                />
+              </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </Layout>
+    </>
+  );
+};
 
-export default Register
+export default Register;
