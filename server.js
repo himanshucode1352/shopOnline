5d45bccf1491607d38d4059272c017d4c94d8ed1
Himanshu <himtri6657@gmail.com>
2023-05-14 21:57:08 +0530

    commit connection frontend backend

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ import colors from "colors";
 import Dotenv from "dotenv"
 import connectDb from "./config/db.js";
 import authRoutes from './routes/authRoutes.js'
+import cors from 'cors'
 //env config
 Dotenv.config()
 
 const app = express()
 //middlewares
 app.use(express.json())
+
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 //connect to database
 connectDb();
@@ -22,5 +29,5 @@ app.get('/',()=>{
 app.use('/user',authRoutes)
 
 // port
-const   port =  process.env.PORT ||8000
+const   port =  process.env.PORT ||8080
 app.listen(port,()=>{console.log(`app is running at ${port}`)});
